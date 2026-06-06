012ea588224ec051221cf66bea0655993cf8d76c
Tavinker <gstavares99@gmail.com>
2026-06-06 19:57:03 -0300

    uploading project

---
+++ readme.md
@@ -0,0 +1,6 @@
+Playwright main commands:
+- install playwright to start a project: npm install -D @playwright/test   
+- run the test file: npx playwright test
+- run the test file with interface: npx playwright test --headed
+- show the test report: npx playwright show-report
+
---
+++ tests/login.spec.js
@@ -0,0 +1,40 @@
+const { test, expect } = require('@playwright/test');
+
+test('1 - acessing the page', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await expect(page).toHaveTitle('Swag Labs');
+});
+
+test('2 - submiting wrong username', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    // await page.waitForTimeout(5000);
+    await page.locator('#user-name').fill('standard_user123');
+    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+});
+
+test('3 - submiting wrong password', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#user-name').fill('standard_user');
+    await page.locator('#password').fill('secret_sauce010203');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+});
+
+test('4 - submiting wrong username and password', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#user-name').fill('standard_user123');
+    await page.locator('#password').fill('secret_sauce010203');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+});
+
+test('5 - submiting correct credentials and doing the login', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#user-name').fill('standard_user');
+    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+});
+
