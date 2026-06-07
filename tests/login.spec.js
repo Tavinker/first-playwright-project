5f9e23f23bff77de23f996cdbfc1fa23bc5b0793
Tavinker <gstavares99@gmail.com>
2026-06-07 19:03:05 -0300

    finishing login scenarios

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,13 +5,27 @@ test('1 - acessing the page', async ({ page }) => {
     await expect(page).toHaveTitle('Swag Labs');
 });
 
-/*adicionar mais cenários:
-- login com campos vazios
-- login somente com username correto preenchido
-- login comente com password correto preenchido
-*/
+test('2 - try to login without credentials (username and password)', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username is required')).toBeVisible();
+});
 
-test('2 - submiting wrong username', async ({ page }) => {
+test('3 - try to login without username', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username is required')).toBeVisible();
+});
+
+test('4 - try to login without password', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Epic sadface: Username is required')).toBeVisible();
+});
+
+test('5 - submiting wrong username', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     // await page.waitForTimeout(5000);
     await page.locator('#user-name').fill('standard_user123');
@@ -20,7 +34,7 @@ test('2 - submiting wrong username', async ({ page }) => {
     await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
 
-test('3 - submiting wrong password', async ({ page }) => {
+test('6 - submiting wrong password', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill('standard_user');
     await page.locator('#password').fill('secret_sauce010203');
@@ -28,7 +42,7 @@ test('3 - submiting wrong password', async ({ page }) => {
     await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
 
-test('4 - submiting wrong username and password', async ({ page }) => {
+test('7 - submiting wrong username and password', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill('standard_user123');
     await page.locator('#password').fill('secret_sauce010203');
@@ -36,11 +50,12 @@ test('4 - submiting wrong username and password', async ({ page }) => {
     await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
 
-test('5 - submiting correct credentials and doing the login', async ({ page }) => {
+test('8 - submiting correct credentials and doing the login', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill('standard_user');
     await page.locator('#password').fill('secret_sauce');
     await page.locator('#login-button').click();
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveTitle('Swag Labs');
 });
 
